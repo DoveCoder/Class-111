686abf3ba78d2d4f08da427df128a1ed7c40ca10
Julian Smith <jmsmith118@yahoo.com>
2021-11-15 15:01:53 -0800

    Assignment-1

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from flask import Flask         #From the flask module import Flask class.
+
+app = Flask(__name__)           # We are instantiating our Flask class into 
+                                # our app variable.
+
+@app.route("/")                 # A decorator that creates a new HTTP path.
+def index():                    # In the context of flask, this is a view function
+    return "<h1>Hello, world!<h1>"
+
+@app.route("/users/newtron")
+def about_newtron():
+    me = {
+        "first_name": "Jimmy",
+        "last_name": "Newtron",
+        "hobbies": "Music",
+        "active": True
+    }
+    return me
+
+@app.route("/greeting/<name>")
+def print_name(name):
+    return "<h1>Hello, %s!<h1>" % name
+
+@app.route("/square/<int:num>")
+def square_num(num):
+    return "<h1>That number, squared, is: %s<h1>" % (num**2)
